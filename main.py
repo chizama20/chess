5f4cfe0095f783b564ff9069485b20683d2e00fe
Chi Ndeh-Zama <chizama20@gmail.com>
2026-02-18 18:59:46 -0500

    just trying to render a board, for sure going into a new branch for this testing

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import chess
+import chess.svg
+
+board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
+
+svg = chess.svg.board(
+    board,
+    fill=dict.fromkeys(board.attacks(chess.E4), "#cc0000cc"),
+    arrows=[chess.svg.Arrow(chess.E4, chess.F6, color="#0000cccc")],
+    squares=chess.SquareSet(chess.BB_DARK_SQUARES & chess.BB_FILE_B),
+    size=350,
+)
+
+with open("board.svg", "w") as f:
+    f.write(svg)
