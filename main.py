5a3b0edb04ead4a9409d69f20b30bff1208cb4d2
Chi Ndeh-Zama <chizama20@gmail.com>
2026-02-20 09:54:38 -0500

    Now have a working game with Engine just deciding on random moves

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import chess
+import random
 
 class Engine:
-    def evaluate():
+    def evaluate(self, board):
         pass
-    def cycle():
-        pass
-    def best_move():
-        pass
+    def best_move(self, board):
+        moveList = list(board.legal_moves)
+        move = random.choice(moveList)
+        return move
 
 def main():
     board = chess.Board()
-    start = True
-
+    engine = Engine()
+    
     while not board.is_game_over():
         print(board)
-        
+        legal = list(board.legal_moves)    
         if board.turn == chess.WHITE:
+            print(legal)
             move = input("Enter a move: ")
             board.push_san(move)
         else:
-            engine = Engine()
             move = engine.best_move(board)
-            board.parse_san(move)
+            board.push(move)
     print("Game Over")
-
 
 if __name__=="__main__":
     main()
