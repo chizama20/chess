6ccfe6533e0a4b9e66a00898b6a7fa444f9121c6
Chi Ndeh-Zama <chizama20@gmail.com>
2026-02-25 19:21:59 -0500

    made a smarter system

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import chess
 import random
-
+# get uci (start pos + end pos) using move.uci()
+# check 
 class Engine:
-    def evaluate(self, board):
-        pass
+    
+    def evaluate(self, board): # evaluate the moves of each piece by score
+        #piecePriceList = { 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, 'k': 20}
+        captureMoves = []
+        for move in board.legal_moves:
+            if board.is_capture(move):
+                captureMoves.append(move)    
+        return captureMoves
+    
+    def get_uci(self, board):
+        fullMoveList = []
+        for move in board.legal_moves:
+            fullMoveList.append(move.uci())
+        return fullMoveList
+
     def best_move(self, board):
-        moveList = list(board.legal_moves)
-        move = random.choice(moveList)
-        return move
+        moveList =  list(board.legal_moves)
+        captureMoveList = self.evaluate(board)
+        if captureMoveList:
+            return random.choice(captureMoveList)
+        else:
+            return random.choice(moveList)
 
 def main():
     board = chess.Board()
