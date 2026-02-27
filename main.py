1fb4691ea6d0ebe7876b6530e336a28af042aaa3
Chi Ndeh-Zama <chizama20@gmail.com>
2026-02-27 03:42:12 -0500

    Made a version 1 for my chess ai. used minmaxing. should add some testing field to. measure how long it took/or how many plays it took. Also research on next algorithm

--- main.py
+++ main.py
@@ -5,28 +5,56 @@ import random
 class Engine:
     
     def evaluate(self, board): # evaluate the moves of each piece by score
-        #piecePriceList = { 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, 'k': 20}
-        captureMoves = []
-        for move in board.legal_moves:
-            if board.is_capture(move):
-                captureMoves.append(move)    
-        return captureMoves
+        pieceVal = { 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, 'k': 20}
+        piecePositions = board.fen().split()[0] # get the piece positions from the FEN string
+        evalScore = 0
+        for piece in piecePositions:
+            if not piece.isalpha(): # if it's not a piece, skip it
+                continue
+            else:
+                if piece.isupper(): # if it's a white piece, get its value
+                    value = pieceVal[piece.lower()]
+                    evalScore += value
+                else: # if it's a black piece, get its value
+                    value = pieceVal[piece]
+                    evalScore -= value 
+        return evalScore
     
-    def get_uci(self, board):
-        fullMoveList = []
-        for move in board.legal_moves:
-            fullMoveList.append(move.uci())
-        return fullMoveList
-
-    def best_move(self, board):
-        moveList =  list(board.legal_moves)
-        captureMoveList = self.evaluate(board)
-        if captureMoveList:
-            return random.choice(captureMoveList)
+    def minimax(self, board, depth, maximizingPlayer):
+        if depth == 0 or board.is_game_over():
+            return self.evaluate(board)
+        
+        if maximizingPlayer:
+            maxEval = float('-inf')
+            for move in board.legal_moves:
+                board.push(move)
+                eval = self.minimax(board, depth - 1, False)
+                board.pop()
+                maxEval = max(maxEval, eval)
+            return maxEval
         else:
-            return random.choice(moveList)
+            minEval = float('inf')
+            for move in board.legal_moves:
+                board.push(move)
+                eval = self.minimax(board, depth - 1, True)
+                board.pop()
+                minEval = min(minEval, eval)
+            return minEval
 
+    def best_move(self, board, depth=3):
+        bestMove = None
+        bestScore = float('-inf')
+        for move in board.legal_moves:
+            board.push(move)
+            score = self.minimax(board, depth - 1, False)
+            board.pop()
+            if score > bestScore:
+                bestScore = score
+                bestMove = move
+        return bestMove
+        
 def main():
+    
     board = chess.Board()
     engine = Engine()
     
@@ -34,13 +62,18 @@ def main():
         print(board)
         legal = [board.legal_moves]    
         if board.turn == chess.WHITE:
+            """
             print(legal)
             move = input("Enter a move: ")
             board.push_san(move)
+            """
+            move = random.choice(list(board.legal_moves))
+            board.push(move)
         else:
             move = engine.best_move(board)
             board.push(move)
     print("Game Over")
+    
 
 if __name__=="__main__":
     main()
